0a2b13b8b1989711bccca6d96dc8a1abc78dfc12
prv2023 <153763357+prv2023@users.noreply.github.com>
2023-12-17 19:52:25 +0530

    51_count.sh

---
+++ shellscript/51_count.sh
@@ -0,0 +1,11 @@
+"D:\djsanghvi\coding\sf\big3.sh"=$1
+word_count=$(wc -w < "D:\djsanghvi\coding\sf\big3.sh")
+char_count=$(wc -m < "D:\djsanghvi\coding\sf\big3.sh")
+line_count=$(wc -l < "D:\djsanghvi\coding\sf\big3.sh")
+
+# Display the counts
+echo "Word count: $word_count"
+echo "Character count: $char_count"
+echo "Line count: $line_count"
+
+#======= countwords51.sh =====
